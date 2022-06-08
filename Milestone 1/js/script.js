9288d2c8b104b8f4f621207a4929a09ebb2f1f65
Matpas97 <passarellim22@gmail.com>
2022-06-08 16:45:26 +0200

    Milestone 1

--- Milestone 1/js/script.js
+++ Milestone 1/js/script.js
@@ -5,17 +5,20 @@
 // Stampare in console.
 
 //Stampo i numeri da 1 a 100
+ // Aggiungo Fizz se multiplo di 3 , Buzz se moltiplo di 5
 
-for(i = 1 ; i <= 100 ; i++); {
-    if(i % 3 === 0); {
-        console.log('Fizz');
-    }
-    
-    if(i % 5 === 0); {
-        console.log('Buzz');
-    }
-   
-   if(i % 3 === 0, i % 5 === 0); {
+for(let i = 1; i <= 100; i++) {
+   if(i % 3 === 0 && i % 5 === 0) {
        console.log('FizzBuzz');
    }
+   else if( i % 3 === 0) {
+       console.log('Fizz');
+   }
+
+else if( i % 5 === 0) {
+    console.log('Buzz');
+}
+else {
+    console.log(i);
+}
 }
